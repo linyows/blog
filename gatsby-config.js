4fa2e9179ffc7c6381e8b7378998e020c3362059
linyows <linyows@gmail.com>
2020-04-26 11:15:47 +0900

    support PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-source-graphql",
       options: {
@@ -39,6 +40,32 @@ module.exports = {
       options: {
         color: `#888`,
         showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `linyows`,
+        short_name: `linyows`,
+        start_url: `/`,
+        background_color: `#333`,
+        theme_color: `#333`,
+        display: `minimal-ui`,
+        // https://favicon.io/favicon-generator/
+        // Background: Rounded, Font: Gio, Size: 110
+        icon: `static/apple-touch-icon.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `static/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `static/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
   ],
